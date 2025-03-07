3f20d2e8bf6769537b713c6107b1dbc2c6a856e8
100495680 <100495680@alumnos.uc3m.es>
2025-03-07 09:57:48 +0100

    JuJa

--- app-cliente1.c
+++ app-cliente1.c
@@ -2,7 +2,7 @@
 #include "claves.h"
 
 int main() {
-    int key = 10;
+    int key = 11;
     char v1[MAX_STRING] = "Ejemplo";
     double v2[MAX_VECTOR] = {1.1, 2.2, 3.3};
     struct Coord v3 = {5, 6};
@@ -43,35 +43,6 @@ int main() {
         }
     }
 
-
-    // Prueba de modify value
-    printf("Modificando clave...\n");
-    strncpy(v1, "Ejemplo modificado", MAX_STRING - 1);
-    response = modify_value(key, v1, 3, v2, v3);
-    if (response == 0) {
-        printf("Clave modificada correctamente: %s\n", v1);
-    } else {
-        if (response == -1) {
-            printf("Error al obtener clave\n");
-        }
-        if (response == -2) {
-            printf("Error en el servidor\n");
-        }
-    }
-
-    // Prueba de delete value
-    printf("Borrando clave...\n");
-    response = delete_key(key);
-    if (response == 0) {
-        printf("Clave borrada correctamente\n");
-    } else {
-        if (response == -1) {
-            printf("Error al obtener clave\n");
-        }
-        if (response == -2) {
-            printf("Error en el servidor\n");
-        }
-    }
 
     return 0;
 }
